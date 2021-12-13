ee171b935942ede4e72045425e98f9f25e9f0a58
Erik Larson <erik@statropy.com>
2021-12-13 10:17:12 -0500

    Day 12 Python : add multiprocessing

--- day12.py
+++ day12.py
@@ -1,5 +1,7 @@
 #day12.py 2021
 import unittest
+from multiprocessing import Pool
+import itertools
 
 def get_map(lines):
     edges = set()
@@ -49,6 +51,19 @@ def part2(lines):
             traverse(edges, node, '', solutions, dup)
     return len(solutions)
 
+def part2worker(edges, lower, node):
+    solution = set()
+    for dup in lower:
+        traverse(edges, node, '', solution, dup)
+    return solution
+
+def part2mp(lines):
+    edges, starts, lower = get_map(lines)
+    with Pool(len(starts)) as p:
+        result = p.starmap(part2worker, zip(itertools.repeat(edges), itertools.repeat(lower), starts))
+        solution = set().union(*result)
+        return len(solution)
+
 class TestDay12(unittest.TestCase):
     def test_1a(self):
         with open('./test12.txt', 'r') as f:
@@ -73,6 +88,14 @@ class TestDay12(unittest.TestCase):
     def test_2(self):
         with open('./input12.txt', 'r') as f:
             self.assertEqual(part2(list(f)), 116985)
+
+    def test_2a_mp(self):
+        with open('./test12.txt', 'r') as f:
+            self.assertEqual(part2mp(list(f)), 36)
+
+    def test_2_mp(self):
+        with open('./input12.txt', 'r') as f:
+            self.assertEqual(part2mp(list(f)), 116985)
 
 if __name__ == '__main__':
     unittest.main()
